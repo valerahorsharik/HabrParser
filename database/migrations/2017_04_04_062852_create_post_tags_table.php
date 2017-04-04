a2774e1c334d717b86b1fb68378b497c34b9ebcb
valerahorsharik <valera.horsharik@gmail.com>
2017-04-04 12:43:18 +0300

    add id

--- database/migrations/2017_04_04_062852_create_post_tags_table.php
+++ database/migrations/2017_04_04_062852_create_post_tags_table.php
@@ -14,6 +14,7 @@ class CreatePostTagsTable extends Migration
     public function up()
     {
         Schema::create('post_tags', function (Blueprint $table) {
+            $table->increments('id');
             $table->integer('post_id');
             $table->integer('tag_id');
         });
