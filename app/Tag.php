fc3378d403fb8b8427155f5411f7a006a2fdcc48
valerahorsharik <valera.horsharik@gmail.com>
2017-04-04 12:42:49 +0300

    Turn off timestamps

--- app/Tag.php
+++ app/Tag.php
@@ -6,6 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'tag',
+    ];
+    
+     /**
+     *
+     * Turn off timestamps
+     * 
+     * @var bool
+     */
+    public $timestamps = false;
     
     public function posts(){
         return $this->belongsToMany('App\Post', 'post_tags');
